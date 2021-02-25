72f99a8ac2eadb1cbe3b15826ec605c403fad7a0
lucas <lucassouza690@gmail.com>
2021-02-25 20:59:49 -0300

    61. 0705 Ajustando o Controller

--- 02 - RestWithAsp-NET5_QuatroVerbos/RestWithAsp-NET5/Startup.cs
+++ 02 - RestWithAsp-NET5_QuatroVerbos/RestWithAsp-NET5/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestWithAsp_NET5.Services;
+using RestWithAsp_NET5.Services.Implementations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,6 +29,7 @@ namespace RestWithAsp_NET5
     {
 
       services.AddControllers();
+      services.AddScoped<IPersonService, PersonServiceImplementation>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
