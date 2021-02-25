ce1375e8289a8888e266f3f0cd743b21948c6a88
lucas <lucassouza690@gmail.com>
2021-02-25 20:20:06 -0300

    59. 0703 Definindo a Interface IPersonService

---
+++ 02 - RestWithAsp-NET5_QuatroVerbos/RestWithAsp-NET5/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestWithAsp_NET5.Model;
+using System.Collections.Generic;
+
+namespace RestWithAsp_NET5.Services
+{
+  public interface IPersonService
+  {
+    Person Create(Person person); 
+    Person FindByID(long id); 
+    List<Person> FindAll(); 
+    Person Update(Person person); 
+    void Delete(long id); 
+  }
+}
