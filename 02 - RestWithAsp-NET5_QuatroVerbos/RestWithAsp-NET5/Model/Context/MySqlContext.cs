37c924d7a532aed99a45b44cfbbd99af129679ba
lucas <lucassouza690@gmail.com>
2021-02-28 11:25:58 -0300

    66. 0803 Definindo as Propriedades de Conexão com a Base de Dados

---
+++ 02 - RestWithAsp-NET5_QuatroVerbos/RestWithAsp-NET5/Model/Context/MySqlContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAsp_NET5.Model.Context
+{
+  public class MySqlContext : DbContext
+  {
+    public MySqlContext() { }
+    public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) { }
+    public DbSet<Person> Persons { get; set; }
+  }
+}
