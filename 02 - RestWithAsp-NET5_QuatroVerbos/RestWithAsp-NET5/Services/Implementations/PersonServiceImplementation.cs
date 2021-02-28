f51bea7c5cd898c66dc0f6c774d62109fc3d253e
lucas <lucassouza690@gmail.com>
2021-02-28 11:51:26 -0300

    69. 0806 Removendo os Mocks Restantes

--- 02 - RestWithAsp-NET5_QuatroVerbos/RestWithAsp-NET5/Services/Implementations/PersonServiceImplementation.cs
+++ 02 - RestWithAsp-NET5_QuatroVerbos/RestWithAsp-NET5/Services/Implementations/PersonServiceImplementation.cs
@@ -17,37 +17,70 @@ namespace RestWithAsp_NET5.Services.Implementations
       _context = context;
     }
 
-    public Person Create(Person person)
-    {
-      return person;
-    }
-
-    public void Delete(long id)
-    {
-
-    }
-
     public List<Person> FindAll()
     {
       return _context.Persons.ToList();
     }
 
-
     public Person FindByID(long id)
     {
-      return new Person
+      return _context.Persons.SingleOrDefault(x => x.Id.Equals(id));
+    }
+
+    public Person Create(Person person)
+    {
+      try
       {
-        Id = 1,
-        FirstName = "Lucas",
-        LastName = "Souza",
-        Address = "Lafaiete",
-        Gender = "Male",
-      };
+        _context.Add(person);
+        _context.SaveChanges();
+      }
+      catch(Exception)
+      {
+        throw;
+      }
+      return person;
     }
 
     public Person Update(Person person)
     {
-      return person ;
+      if (!Exists(person.Id)) return new Person();
+      var result = _context.Persons.SingleOrDefault(x => x.Id.Equals(person.Id));
+      if (result != null)
+      {
+        try
+        {
+          _context.Entry(result).CurrentValues.SetValues(person);
+          _context.SaveChanges();
+        }
+        catch (Exception)
+        {
+          throw;
+        }
+      }
+
+      return person;
+    }
+
+    public void Delete(long id)
+    {
+      var result = _context.Persons.SingleOrDefault(x => x.Id.Equals(id));
+      if (result != null)
+      {
+        try
+        {
+          _context.Persons.Remove(result);
+          _context.SaveChanges();
+        }
+        catch (Exception)
+        {
+          throw;
+        }
+      }
+    }
+
+    private bool Exists(long id)
+    {
+      return _context.Persons.Any(x => x.Id.Equals(id));
     }
   }
 }
