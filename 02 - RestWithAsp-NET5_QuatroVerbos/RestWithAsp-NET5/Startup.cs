d9ebaf5ddeef47a17921f978ea77ba71aed3c5bc
lucas <lucassouza690@gmail.com>
2021-02-28 11:37:59 -0300

    67. 0804 Finalizando as Propriedades de Conexão com a Base de Dados

--- 02 - RestWithAsp-NET5_QuatroVerbos/RestWithAsp-NET5/Startup.cs
+++ 02 - RestWithAsp-NET5_QuatroVerbos/RestWithAsp-NET5/Startup.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestWithAsp_NET5.Model.Context;
 using RestWithAsp_NET5.Services;
 using RestWithAsp_NET5.Services.Implementations;
 using System;
@@ -29,6 +31,9 @@ namespace RestWithAsp_NET5
     {
 
       services.AddControllers();
+      var connection = Configuration["MySqlConnection:MySqlConnectionString"];
+      services.AddDbContext<MySqlContext>(options => options.UseMySql(connection));
+
       services.AddScoped<IPersonService, PersonServiceImplementation>();
     }
 
