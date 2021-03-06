e28f361c273cf5c57574f08648f86ea7b68755de
lucas <lucassouza690@gmail.com>
2021-03-06 10:22:28 -0300

    90. 1204 Implementando a Estrutura Base do Repositório Genérico

---
+++ 02 - RestWithAsp-NET5_QuatroVerbos/RestWithAsp-NET5/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,41 @@
+﻿using RestWithAsp_NET5.Model.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAsp_NET5.Repository.Generic
+{
+  public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+  {
+    public T Create(T item)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Delete(long id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public bool Exists(long id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public List<T> FindAll()
+    {
+      throw new NotImplementedException();
+    }
+
+    public T FindByID(long id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public T Update(T item)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
