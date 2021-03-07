2233b4d78d08fb4470f19fb28ea7dc15a2910573
lucas <lucassouza690@gmail.com>
2021-03-07 17:08:46 -0300

    99. 1304 Ajustando o Business para Converter VO para Entidade

--- 02 - RestWithAsp-NET5_QuatroVerbos/RestWithAsp-NET5/Business/IPersonBusiness.cs
+++ 02 - RestWithAsp-NET5_QuatroVerbos/RestWithAsp-NET5/Business/IPersonBusiness.cs
@@ -1,14 +1,14 @@
-﻿using RestWithAsp_NET5.Model;
+﻿using RestWithAsp_NET5.Data.VO;
 using System.Collections.Generic;
 
 namespace RestWithAsp_NET5.Business
 {
   public interface IPersonBusiness
   {
-    Person Create(Person person); 
-    Person FindByID(long id); 
-    List<Person> FindAll(); 
-    Person Update(Person person); 
+    PersonVO Create(PersonVO person);
+    PersonVO FindByID(long id); 
+    List<PersonVO> FindAll();
+    PersonVO Update(PersonVO person); 
     void Delete(long id); 
   }
 }
--- 02 - RestWithAsp-NET5_QuatroVerbos/RestWithAsp-NET5/Controllers/PersonController.cs
+++ 02 - RestWithAsp-NET5_QuatroVerbos/RestWithAsp-NET5/Controllers/PersonController.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RestWithAsp_NET5.Data.VO;
 
 namespace RestWithAsp_NET5.Controllers
 {
@@ -40,7 +41,7 @@ namespace RestWithAsp_NET5.Controllers
     }
 
     [HttpPost]
-    public IActionResult Post([FromBody] Person person)
+    public IActionResult Post([FromBody] PersonVO person)
     {
       if (person == null)
         return BadRequest();
@@ -48,7 +49,7 @@ namespace RestWithAsp_NET5.Controllers
     }
 
     [HttpPut]
-    public IActionResult Put([FromBody] Person person)
+    public IActionResult Put([FromBody] PersonVO person)
     {
       if (person == null)
         return BadRequest();
