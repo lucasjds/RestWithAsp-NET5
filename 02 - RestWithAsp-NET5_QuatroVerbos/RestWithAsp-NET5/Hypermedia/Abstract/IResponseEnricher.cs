6f942f7329055ba30740e8f4865f61cce6982ba5
lucas <lucassouza690@gmail.com>
2021-03-09 21:44:41 -0300

    110. 1603 Criando as Interfaces e Constantes

---
+++ 02 - RestWithAsp-NET5_QuatroVerbos/RestWithAsp-NET5/Hypermedia/Abstract/IResponseEnricher.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Threading.Tasks;
+
+namespace RestWithAsp_NET5.Hypermedia.Abstract
+{
+  public interface IResponseEnricher
+  {
+    bool CanEnrich(ResultExecutedContext context);
+    Task Enrich(ResultExecutedContext context);
+  }
+}
---
+++ 02 - RestWithAsp-NET5_QuatroVerbos/RestWithAsp-NET5/Hypermedia/Abstract/ISupportHypermedia.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RestWithAsp_NET5.Hypermedia.Abstract
+{
+  public interface ISupportHypermedia
+  {
+    List<HyperMediaLink> Links { get; set; }
+  }
+}
