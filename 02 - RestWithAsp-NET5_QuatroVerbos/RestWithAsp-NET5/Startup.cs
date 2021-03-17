978b449011b0096575442e6a90a89a540993656c
lucas <lucassouza690@gmail.com>
2021-03-17 20:48:08 -0300

    123. 1802 Habilitando CORS

--- 02 - RestWithAsp-NET5_QuatroVerbos/RestWithAsp-NET5/Startup.cs
+++ 02 - RestWithAsp-NET5_QuatroVerbos/RestWithAsp-NET5/Startup.cs
@@ -37,7 +37,10 @@ namespace RestWithAsp_NET5
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-     
+      services.AddCors(options => options.AddDefaultPolicy(builder =>
+      {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+      }));
       services.AddControllers();
       var connection = Configuration["MySqlConnection:MySqlConnectionString"];
       services.AddDbContext<MySqlContext>(options => options.UseMySql(connection));
@@ -108,6 +111,8 @@ namespace RestWithAsp_NET5
       app.UseHttpsRedirection();
 
       app.UseRouting();
+
+      app.UseCors();
 
       app.UseSwagger();
       app.UseSwaggerUI(c => {
