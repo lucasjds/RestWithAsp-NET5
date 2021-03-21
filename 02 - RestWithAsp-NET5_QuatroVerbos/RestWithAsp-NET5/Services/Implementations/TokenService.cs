2a85164649c6889b09be89ead1e961c99dbae1f4
lucas <lucassouza690@gmail.com>
2021-03-21 10:39:05 -0300

    129. 1905 Implementando o TokenService

---
+++ 02 - RestWithAsp-NET5_QuatroVerbos/RestWithAsp-NET5/Services/Implementations/TokenService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using RestWithAsp_NET5.Configurations;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestWithAsp_NET5.Services.Implementations
+{
+  public class TokenService : ITokenInterface
+  {
+    private TokenConfiguration _tokenConfiguration;
+
+    public TokenService(TokenConfiguration tokenConfiguration)
+    {
+      _tokenConfiguration = tokenConfiguration;
+    }
+
+    public string GenerateAccessToken(IEnumerable<Claim> claims)
+    {
+      var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_tokenConfiguration.Secret));
+      var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+      var tokenOptions = new JwtSecurityToken(
+          issuer: _tokenConfiguration.Issuer,
+          audience: _tokenConfiguration.Audience,
+          claims : claims,
+          expires : DateTime.Now.AddMinutes(_tokenConfiguration.Minutes),
+          signingCredentials: signinCredentials
+        );
+      return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+    }
+
+    public string GenerateRefreshToken()
+    {
+      throw new NotImplementedException();
+    }
+
+    public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
