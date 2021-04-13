fcc932f513a1130a1628306da9bce77e9912e074
lucas <lucassouza690@gmail.com>
2021-04-13 20:58:29 -0300

    166. 2304 Adicionando o Suporte ao Docker

--- 02 - RestWithAsp-NET5_QuatroVerbos/RestWithAsp-NET5/obj/Debug/net5.0/RestWithAsp-NET5.AssemblyInfo.cs
+++ 02 - RestWithAsp-NET5_QuatroVerbos/RestWithAsp-NET5/obj/Debug/net5.0/RestWithAsp-NET5.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("144f6b6a-1a5b-4830-b3da-e23b286c3676")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestWithAsp-NET5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
