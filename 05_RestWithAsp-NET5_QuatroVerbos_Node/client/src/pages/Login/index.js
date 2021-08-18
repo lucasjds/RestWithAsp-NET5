8e22ba3de6b1478496e6f3763a0123f14ab91c59
lucas <lucassouza690@gmail.com>
2021-08-18 19:44:36 -0300

    216. 2712 Definindo a Estrutura HTML da Tela de Login

--- 05_RestWithAsp-NET5_QuatroVerbos_Node/client/src/pages/Login/index.js
+++ 05_RestWithAsp-NET5_QuatroVerbos_Node/client/src/pages/Login/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import './style.css';
 
+import logoImage from '../../assets/logo.svg';
+import padlock from '../../assets/padlock.png';
+
+
 export default function Login(){
-    return (<h1>Welcome</h1>);
+    return (
+        <div className="login-container">
+            <section className="form">
+                <img src={logoImage} alt="Erudio logo"/>
+                <form>
+                    <h1>Access your account</h1>
+                    <input placeholder="username"></input>
+                    <input type="password" placeholder="password"></input>
+                    <button type="submit">Login</button>
+                </form>
+            </section>
+            <img src={padlock} alt="Login"></img>
+        </div>
+    );
 }
