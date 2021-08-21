d3f8f657d80fcfed6c4ed7193b5587941427b9e7
lucas <lucassouza690@gmail.com>
2021-08-20 21:47:16 -0300

    219. 2715 Iniciando a Implementação da Tela de Livros

--- 05_RestWithAsp-NET5_QuatroVerbos_Node/client/src/routes.js
+++ 05_RestWithAsp-NET5_QuatroVerbos_Node/client/src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Login from './pages/Login';
+import Book from './pages/Book';
 
 export default function Routes(){
     return (
         <BrowserRouter>
             <switch>
-                <Route pat="/" component={Login}/>
+                <Route path="/" exact component={Login}/>
+                <Route path="/book" exact component={Book}/>
             </switch>
         </BrowserRouter>
 
