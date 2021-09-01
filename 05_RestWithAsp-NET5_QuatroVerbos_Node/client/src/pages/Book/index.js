41d3ff69c2e5a5f17fe63c370009587ca663f39c
lucas <lucassouza690@gmail.com>
2021-08-31 21:51:17 -0300

    221. 2717 Estilizando o Cabeçalho da Tela de Livros

--- 05_RestWithAsp-NET5_QuatroVerbos_Node/client/src/pages/Book/index.js
+++ 05_RestWithAsp-NET5_QuatroVerbos_Node/client/src/pages/Book/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
+import {FiPower} from 'react-icons/fi';
+import logoImage from '../../assets/logo.svg';
 import './style.css';
 
 export default function Book(){
-    return <h1>Book</h1>;
+    return (
+        <div className="book-container">
+            <header>
+                <img src={logoImage} alt="Erudio"/>
+                <span>Welcome, <strong>Lucas</strong>!</span>
+                <Link className="button" to="book/new">Add new book</Link>
+                <button type="button"><FiPower size={18} color="#251FC5"/></button>
+            </header>
+        </div>
+    );
 }
